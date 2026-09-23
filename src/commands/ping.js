fc8839df05d5fa77dce76692db9789e5b3f76898
Paradox Tech <esanallen61@gmail.com>
2026-09-23 10:53:24 +0100

    chore: remove flat ping.js

--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Check if the bot is alive",
-  async execute({ m }) {
-    const start = Date.now();
-    await m.reply(`Pong 🏓\nLatency: ${Date.now() - start}ms`);
-  },
-};
